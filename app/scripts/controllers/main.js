d663a24e6b6798a80b15fdcf5f72b6646c4fcd55
aurigadl <aurigadl@gmail.com>
2013-12-24 13:07:38 -0500

    New file

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('webodfAngularjsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
